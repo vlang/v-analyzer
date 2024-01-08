4f95ceb2a8455cd3c37f113d55e0be8741aec9f7
Lycs-D <139197676+Lycs-D@users.noreply.github.com>
2024-01-09 05:28:01 +0800

    [tree_sitter_v] update and clean

--- tree_sitter_v/bindings/v/bindings.h
+++ tree_sitter_v/bindings/v/bindings.h
@@ -1,3 +1,16 @@
-#include "tree_sitter/parser.h"
+#ifndef TREE_SITTER_V_H_
+#define TREE_SITTER_V_H_
 
-TSLanguage * tree_sitter_v();
+#include <tree_sitter/parser.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern TSLanguage *tree_sitter_v();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_V_H_
