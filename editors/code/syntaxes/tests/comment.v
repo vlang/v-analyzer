59d104a4f3a860c71bdd93f6c897a2139b0f369b
Lycs-D <139197676+Lycs-D@users.noreply.github.com>
2024-03-17 00:06:05 +0800

    [editors/code] Add shebang syntaxe (#34)

---
+++ editors/code/syntaxes/tests/comment.v
@@ -0,0 +1,5 @@
+// SYNTAX TEST "source.v" "comment"
+   #!/usr/bin/env -S v
+// ^^ punctuation.definition.comment.shebang.v
+//   ^^^^^^^^^^^^^^^^^ meta.shebang.v
+// ^^^^^^^^^^^^^^^^^^^ comment.line.number-sign.v
