70c3b14268202b641e24bd16600343635ef25097
ttytm <tobmoj@web.de>
2024-03-25 16:38:42 +0100

    Revert "temp change to test workflow cancelation"
    This reverts commit 9ae9c771323025de40999e7596ad04ede0965e94.

--- src/main.v
+++ src/main.v
@@ -15,7 +15,6 @@ import metadata
 
 // default_tcp_port is default TCP port that the analyzer uses to connect to the socket
 // when the --socket flag is passed at startup.
-//
 // See also the `--port` flag to specify a custom port.
 const default_tcp_port = 5007
 
