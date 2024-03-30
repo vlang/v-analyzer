103fc33d66e95a033f976057c67e3c002b9b34a3
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-03-30 17:28:26 +0100

    test stub hover documentation

--- src/tests/documentation.v
+++ src/tests/documentation.v
@@ -6,6 +6,7 @@ fn documentation() testing.Tester {
 	mut t := testing.with_name('documentation')
 
 	t.documentation_test('rendered', 'documentation/rendered.v')
+	t.documentation_test('stubs', 'documentation/stubs.v')
 
 	return t
 }
