2584e86d57309e5937c99f297f63b01b83e37262
Delyan Angelov <delian66@gmail.com>
2024-06-14 23:03:48 +0300

    fix behaviour of pascal_case_to_snake_case after V commit 5329a0a67

--- src/utils/text_utils.v
+++ src/utils/text_utils.v
@@ -3,7 +3,7 @@ module utils
 pub fn pascal_case_to_snake_case(s string) string {
 	mut res := ''
 	for index, c in s {
-		if c.ascii_str().is_upper() {
+		if c.ascii_str().is_upper() || c.is_digit() {
 			if index > 0 {
 				res += '_'
 			}
