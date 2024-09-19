7cf79989b6503f12f6803c5e197d68b182ddfa23
Laurent Cheylus <foxy@free.fr>
2024-09-19 12:46:07 +0200

    analyzer: create vtmp directory for check-updates/up commands
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- src/check-updates.v
+++ src/check-updates.v
@@ -1,8 +1,13 @@
 module main
 
 import cli
+import os
 
 fn check_updates_cmd(_ cli.Command) ! {
+	if !os.exists(analyzer_dir) {
+		os.mkdir(analyzer_dir)!
+	}
+
 	download_install_vsh()!
 	call_install_vsh('check-updates')!
 }
--- src/up.v
+++ src/up.v
@@ -4,10 +4,15 @@ import cli
 import term
 import os
 
+pub const analyzer_dir = os.join_path(os.vtmp_dir(), 'v-analyzer')
 pub const analyzer_install_script_download_path = 'https://raw.githubusercontent.com/vlang/v-analyzer/main/install.vsh'
-pub const analyzer_install_script_path = os.join_path(os.vtmp_dir(), 'v-analzyer', 'install.vsh')
+pub const analyzer_install_script_path = os.join_path(analyzer_dir, 'install.vsh')
 
 fn up_cmd(cmd cli.Command) ! {
+	if !os.exists(analyzer_dir) {
+		os.mkdir(analyzer_dir)!
+	}
+
 	download_install_vsh()!
 
 	is_nightly := cmd.flags.get_bool('nightly') or { false }
