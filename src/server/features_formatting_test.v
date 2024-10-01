eec7b35a376b4739dd63fcb3d04367cd43992af7
phcreery <phcreery@gmail.com>
2024-10-01 13:58:12 -0500

    update tests

--- src/server/features_formatting_test.v
+++ src/server/features_formatting_test.v
@@ -6,26 +6,18 @@ import analyzer
 import analyzer.psi
 import analyzer.parser
 
-
-
-
 fn test_large_file() {
-
-
 	test_file := os.norm_path(@VMODROOT + '/src/server/general.v')
-	// test_file := os.norm_path(@VEXEROOT + '/vlib/os/process_windows.c.v')
-	// test_file := 'file:///c%3A/Users/phcre/Documents/v/imageeditor/testing/proc.v'
-	dump(test_file)
 
 	src := os.read_file(test_file) or { panic('Cannot read file') }
-	// dump(src)
+
+	mut ls := LanguageServer.new(analyzer.IndexingManager.new())
+	ls.setup_toolchain()
+	ls.setup_vpaths()
 
 	uri := lsp.document_uri_from_path(test_file)
 	res := parser.parse_code(src)
 	psi_file := psi.new_psi_file(uri.path(), res.tree, res.source_text)
-
-
-	mut ls := LanguageServer.new(analyzer.IndexingManager.new())
 	ls.opened_files[uri] = analyzer.OpenedFile{
 		uri:      uri
 		version:  0
@@ -39,7 +31,5 @@ fn test_large_file() {
 	}
 
 	text_edit_result := ls.formatting(params) or { panic('Cannot format file') }
-	dump(text_edit_result)
 	assert text_edit_result.len == 1
-
 }
