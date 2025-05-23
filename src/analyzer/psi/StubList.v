0f963ca4c0cc888252f5a31f37c974d5e2527d06
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2025-05-23 09:44:18 +0800

    fix(#152): prevent invalid memory access on startup (#163)

--- src/analyzer/psi/StubList.v
+++ src/analyzer/psi/StubList.v
@@ -27,7 +27,7 @@ fn (mut s StubList) add_stub(mut stub StubBase, parent &StubElement) {
 	s.index_map[stub_id] = stub
 
 	// add stub to parent's children
-	parent_id := if parent is StubBase { parent.id } else { -1 }
+	parent_id := if parent != unsafe { nil } && parent is StubBase { parent.id } else { -1 }
 	mut parent_children := s.child_map[parent_id]
 	parent_children << stub_id
 	s.child_map[parent_id] = parent_children
